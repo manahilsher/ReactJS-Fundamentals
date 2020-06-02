a56a99f3b74b4f5bd8abbe321359030bbdf954cd
Manahil Sher <manahil.sher@gmail.com>
2020-06-02 18:30:34 -0400

    Updates

---
+++ precache-manifest.4b115c6c221fe0d5486323f791a76051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399ebf0155f190a5d21184a29ceed3f1",
+    "url": "/ttp-contact-card/index.html"
+  },
+  {
+    "revision": "58ab24b4e843c0aac4a8",
+    "url": "/ttp-contact-card/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b85cc55ad15323614ad4",
+    "url": "/ttp-contact-card/static/js/2.479e689c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ttp-contact-card/static/js/2.479e689c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ab24b4e843c0aac4a8",
+    "url": "/ttp-contact-card/static/js/main.908ee0ce.chunk.js"
+  },
+  {
+    "revision": "1ce55cb1b07f9464931b",
+    "url": "/ttp-contact-card/static/js/runtime-main.c2f2bc6e.js"
+  }
+]);
